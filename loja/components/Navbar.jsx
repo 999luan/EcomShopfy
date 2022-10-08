53640663f49a95a7872c084ff3ca91c6feefe6aa
Luan <tatalc4e@gmail.com>
2022-10-08 02:54:35 -0300

    final ver 1.0

---
+++ loja/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import { Cart } from "./";
+import { useStateContext } from "../context/StateContext";
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+  return (
+    <div className="navbar-container">
+      <Link href="/">
+        <h3 style={{ color: "#DF0101", fontWeight: "bold" }}>
+          <p className="logo">99 STORE</p>
+        </h3>
+      </Link>
+
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
